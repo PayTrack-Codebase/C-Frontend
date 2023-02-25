563dda08ff880b9e68f88587d09ec043394bd73e
GREENFONTS <godwillonyewuchii@gmail.com>
2023-02-25 23:45:23 +0100

    feat: started the auth flow

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import "./App.css";
+import AuthLayout from "./Layouts/AuthLayout";
+import Login from "./Pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route element={<AuthLayout />} path="/">
+          <Route element={<Login />} path="/login" />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Layouts/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../Styles/Header.scss";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="content">
+        <p>Don’t have an account?</p>
+        <button>Get Started </button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
